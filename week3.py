1810112e5cd42239740c947b61800417eda2cc4b
ljcmu <ljmusgrove@mavs.coloradomesa.edu>
2026-02-07 18:08:54 -0700

    Add files via upload

---
+++ week3.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:       Section 2.14 Problem 1 - your first assignment
+# Purpose:    Variables and statements
+#
+# Author:      liam
+#
+# Created:     02/07/2026
+# Copyright:   (c) liamj 2026
+# Licence:     Open Source
+#-------------------------------------------------------------------------------
+word1 = "All"
+word2 = " work"
+word3 = " and"
+word4 = " no"
+word5 = " play"
+word6 = " makes"
+word7 = " Jack"
+word8 = " a"
+word9 = " dull"
+word10 = " boy."
+
+print(word1 + word2 + word3 + word4 + word5 + word6 + word7 + word8 + word9 + word10)
+
+
