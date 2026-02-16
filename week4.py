3cdf0180b671c2fb7180e02e33d46f253d482971
ljcmu <ljmusgrove@mavs.coloradomesa.edu>
2026-02-15 22:30:08 -0700

    Add files via upload

---
+++ week4.py
@@ -0,0 +1,41 @@
+
+#-------------------------------------------------------------------------------
+# Name:        Startest
+# Purpose:     Educational
+#
+# Author:      Liam
+#
+# Created:     02/15/2026
+# Copyright:   (c) liamm 2026
+# Licence:     Open Source
+#-------------------------------------------------------------------------------
+import turtle
+wn = turtle.Screen()
+wn.title("Startest")
+
+tess = turtle.Turtle()
+tess.color("blue")
+tess.pensize(5)
+
+tess.forward(250)
+tess.right(130)
+tess.forward(200)
+tess.right(100)
+tess.forward(200)
+tess.right(130)
+tess.forward(75)
+tess.left(70)
+tess.forward(150)
+tess.right(140)
+tess.forward(380)
+
+tess.right(135)
+tess.forward(145)
+
+tess.left(50)
+
+tess.forward(140)
+
+tess.right(135)
+
+tess.forward(250)
