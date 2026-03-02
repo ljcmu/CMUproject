d2e93fb5e6b10137072ec708a817ba801c00e68e
ljcmu <ljmusgrove@mavs.coloradomesa.edu>
2026-03-01 22:11:30 -0700

    Add files via upload
    Mr. Kull I'm having a terrible problem with this file. It shoots me an "Invalid Syntax" error even though I have checked and tweaked every single line of code multiple times within this file. If we could discuss this in class tomorrow that would be excellent as I would like to get at least a "B" on this assignment. Thank you

---
+++ week6.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:       Week 6
+# Purpose:    Conditionals
+#
+# Author:      Liam
+#
+# Created:     03/01/2026
+#-------------------------------------------------------------------------------
+
+def day_prompt(x):
+    if x = 0:
+        print("Sunday")
+        return
+    if x = 1:
+        print("Monday")
+        return
+    if x = 2:
+        print ("Tuesday")
+        return
+    if x = 3:
+        print ("Wednesday")
+        return
+    if x = 4:
+        print ("Thursday")
+        return
+    if x = 5:
+        print ("Friday")
+        return
+    if x = 6:
+        print ("Saturday")
+        return
+
+x = input("Give me a number 0-6. ")
+print (x)
