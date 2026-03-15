5ce8ede96cb98dacc623dabdcde3f5aeb7f7bd44
ljcmu <ljmusgrove@mavs.coloradomesa.edu>
2026-03-15 17:41:42 -0600

    Add files via upload

---
+++ week8.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# Name:        Triangular Numbers Finder
+# Purpose:     Mathematical
+#
+# Author:      Liam
+#
+# Created:     03/15/2026
+# Copyright:   (c) liamm 2026
+# Licence:     Open Source
+#-------------------------------------------------------------------------------
+
+def print_triangular_numbers(x):     # This is the function.
+    for z in range(1, x + 1):
+        print(z, "\t", z * (z + 1) / 2) # This \t function really comes in handy!
+
+print_triangular_numbers(5)     # This gives the first five triangular numbers.
+        
+    
+    
+
+
+    
+    
+
